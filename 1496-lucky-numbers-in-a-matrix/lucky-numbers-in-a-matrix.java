bb339883d25c50c4160a6e87ece029bc9915a34d
Vivek Aravind Tagarampudi <vivektagarampudi@gmail.com>
2026-08-31 22:06:10 +0530

    Time: 2 ms (95.94%) | Memory: 46.9 MB (86.37%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            int min = matrix[i][0];
+            int col = 0;
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                    col = j;
+                }
+            }
+            boolean lucky = true;
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[j][col] > min) {
+                    lucky = false;
+                    break;
+                }
+            }
+            if (lucky) {
+                list.add(min);
+            }
+        }
+        return list;
+    }
+}

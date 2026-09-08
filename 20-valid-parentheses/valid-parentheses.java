40417378242895b31d9f8499f89130a6a28ed3a0
Vivek Aravind Tagarampudi <vivektagarampudi@gmail.com>
2026-09-08 14:15:46 +0530

    Time: 3 ms (86.08%) | Memory: 43.4 MB (40.88%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        ArrayDeque<Character> st = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                char top = st.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}

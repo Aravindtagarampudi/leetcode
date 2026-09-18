a510d8ae34f8d7e449ee77d2f442c281907eff74
Vivek Aravind Tagarampudi <vivektagarampudi@gmail.com>
2026-09-18 21:44:39 +0530

    Time: 2 ms (73.84%) | Memory: 43.2 MB (39.84%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++;
+        }
+        int length = 0;
+        boolean odd = false;
+        for (int i = 0; i < 128; i++) {
+            length += (count[i] / 2) * 2;
+            if (count[i] % 2 == 1) {
+                odd = true;
+            }
+        }
+        if (odd) {
+            length++;
+        }
+        return length;
+    }
+}
